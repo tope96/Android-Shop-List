1ec2fd69c5c26aa74b0b4fb51f82caaf1463f980
tope96 <pietrzaktomek@hotmail.com>
2019-12-16 20:29:50 +0100

    bottom menu not working

--- app/src/main/java/com/example/firstproject/FavouriteShopsListActivity.java
+++ app/src/main/java/com/example/firstproject/FavouriteShopsListActivity.java
@@ -1,7 +1,10 @@
 package com.example.firstproject;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,21 +15,24 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
-public class FavouriteShopsListActivity extends AppCompatActivity {
+public class FavouriteShopsListActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference shopsColl = db.collection("shops");
     private RecyclerAdapterShops adapter;
+    private ActionBar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,97 +42,34 @@ public class FavouriteShopsListActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourite_shops_list);
+        toolbar = getSupportActionBar();
 
-        setUpRecyclerView();
+        loadFragment(new Frag_list());
+        BottomNavigationView navigation = findViewById(R.id.navig);
+        navigation.setOnNavigationItemSelectedListener(this);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        adapter.startListening();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        adapter.stopListening();
     }
 
-    private void setUpRecyclerView(){
-        Query query = shopsColl.orderBy("name", Query.Direction.ASCENDING);
-
-        FirestoreRecyclerOptions<Shop> options = new FirestoreRecyclerOptions.Builder<Shop>()
-                .setQuery(query, Shop.class)
-                .build();
-
-        adapter = new RecyclerAdapterShops(options, this);
-        RecyclerView recyclerView = findViewById(R.id.rvShops);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
 
-        recyclerView.setAdapter(adapter);
-
-        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
-            private Drawable icon;
-            private final ColorDrawable background =  new ColorDrawable(Color.RED);
-
-            @Override
-            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
-                return false;
-            }
-
-            @Override
-            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
-                String documentId = adapter.getDocId(viewHolder.getAdapterPosition());
-                db.collection("products")
-                        .document(documentId)
-                        .delete()
-                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void aVoid) {
-                                adapter.notifyDataSetChanged();
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                            }
-                        });
-            }
-
-            @Override
-            public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
-                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
-
-                icon = getDrawable(R.drawable.ic_delete_forever_black_24dp);
-
-                View itemView = viewHolder.itemView;
-                int backgroundCornerOffset = 20;
-
-                int iconMargin = (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
-                int iconTop = itemView.getTop() + (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
-                int iconBottom = iconTop + icon.getIntrinsicHeight();
-
-                if (dX > 0) { // Swiping to the right
-                    int iconLeft = itemView.getLeft() + iconMargin;
-                    int iconRight = itemView.getLeft() + iconMargin + icon.getIntrinsicWidth();
-                    icon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
-
-                    background.setBounds(itemView.getLeft(), itemView.getTop(),
-                            itemView.getLeft() + ((int) dX) + backgroundCornerOffset, itemView.getBottom());
-                } else if (dX < 0) { // Swiping to the left
-
-                } else {
-                    background.setBounds(0, 0, 0, 0);
-                    icon.setBounds(0, 0, 0, 0);
-                }
-
-                background.draw(c);
-                icon.draw(c);
-            }
-
-        }).attachToRecyclerView(recyclerView);
-
+    private boolean loadFragment(Fragment fragment){
+        if(fragment != null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.frameContainer, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 
     private void changeTheme(boolean dark){
@@ -135,5 +78,22 @@ public class FavouriteShopsListActivity extends AppCompatActivity {
         }else{
             setTheme(R.style.AppTheme);
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+
+        switch(menuItem.getItemId()){
+            case R.id.shopList:
+                fragment = new Frag_list();
+                break;
+
+            case R.id.shopMap:
+                fragment = new Frag_map();
+                break;
+        }
+
+        return loadFragment(fragment);
     }
 }
---
+++ app/src/main/java/com/example/firstproject/Frag_list.java
@@ -0,0 +1,126 @@
+package com.example.firstproject;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+public class Frag_list extends Fragment {
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference shopsColl = db.collection("shops");
+    private RecyclerAdapterShops adapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //just change the fragment_dashboard
+        //with the fragment you want to inflate
+        //like if the class is HomeFragment it should have R.layout.home_fragment
+        //if it is DashboardFragment it should have R.layout.fragment_dashboard
+
+        return inflater.inflate(R.layout.fragment_list, null);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View rootView = inflater.inflate(R.layout.fragment_list, null);
+        setUpRecyclerView(rootView);
+    }
+
+    private void setUpRecyclerView(View view){
+        Query query = shopsColl.orderBy("name", Query.Direction.ASCENDING);
+
+        FirestoreRecyclerOptions<Shop> options = new FirestoreRecyclerOptions.Builder<Shop>()
+                .setQuery(query, Shop.class)
+                .build();
+
+        adapter = new RecyclerAdapterShops(options, getContext());
+        RecyclerView recyclerView = view.findViewById(R.id.rvShopFrag);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        recyclerView.setAdapter(adapter);
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+            private Drawable icon;
+            private final ColorDrawable background =  new ColorDrawable(Color.RED);
+
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                String documentId = adapter.getDocId(viewHolder.getAdapterPosition());
+                db.collection("products")
+                        .document(documentId)
+                        .delete()
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                adapter.notifyDataSetChanged();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                            }
+                        });
+            }
+
+            @Override
+            public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+
+               // icon = getDrawable(R.drawable.ic_delete_forever_black_24dp);
+
+                View itemView = viewHolder.itemView;
+                int backgroundCornerOffset = 20;
+
+                int iconMargin = (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
+                int iconTop = itemView.getTop() + (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
+               // int iconBottom = iconTop + icon.getIntrinsicHeight();
+
+                if (dX > 0) { // Swiping to the right
+                    int iconLeft = itemView.getLeft() + iconMargin;
+                    int iconRight = itemView.getLeft() + iconMargin + icon.getIntrinsicWidth();
+                    //icon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
+
+                    background.setBounds(itemView.getLeft(), itemView.getTop(),
+                            itemView.getLeft() + ((int) dX) + backgroundCornerOffset, itemView.getBottom());
+                } else if (dX < 0) { // Swiping to the left
+
+                } else {
+                    background.setBounds(0, 0, 0, 0);
+                    //icon.setBounds(0, 0, 0, 0);
+                }
+
+                background.draw(c);
+                //icon.draw(c);
+            }
+
+        }).attachToRecyclerView(recyclerView);
+
+    }
+
+}
