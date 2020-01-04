fab200c157be6c96e982824d06ceed7936e39dd7
tope96 <pietrzaktomek@hotmail.com>
2020-01-04 22:42:16 +0100

    all widget works

--- app/src/main/java/com/example/firstproject/listWidget.java
+++ app/src/main/java/com/example/firstproject/listWidget.java
@@ -8,10 +8,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Build;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RemoteViews;
+
+import androidx.annotation.RequiresApi;
 
 /**
  * Implementation of App Widget functionality.
@@ -26,13 +29,15 @@ public class listWidget extends AppWidgetProvider {
     private static final String MyOnClick6 = "prevMusic";
     private static final String MyOnClick7 = "nextMusic";
     public static int[] picArray = new int[]{R.drawable.pepefroggie, R.drawable.im, R.drawable.internettroll};
+    public static int[] soundsArray = new int[]{R.raw.drums, R.raw.strange};
     private static int counter;
-    private static int plays;
+    private static int counterMusic;
     static MediaPlayer mPlay = new MediaPlayer();
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
-        mPlay = MediaPlayer.create(context, R.raw.drums);
+        mPlay = MediaPlayer.create(context, soundsArray[0]);
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.list_widget);
         views.setOnClickPendingIntent(R.id.btGoToWeb, getPendingSelfIntent(context, MyOnClick1));
         views.setOnClickPendingIntent(R.id.btPrev, getPendingSelfIntent(context, MyOnClick2));
@@ -42,17 +47,17 @@ public class listWidget extends AppWidgetProvider {
         views.setOnClickPendingIntent(R.id.btPrevMusic, getPendingSelfIntent(context, MyOnClick6));
         views.setOnClickPendingIntent(R.id.btNextMusic, getPendingSelfIntent(context, MyOnClick7));
         counter = 0;
-    plays =0;
+        counterMusic = 0;
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
-
 
     }
 
@@ -84,13 +89,10 @@ public class listWidget extends AppWidgetProvider {
         }
         if(MyOnClick2.equals(intent.getAction())){
             if(counter == 0){
-                Log.d("Tomek", "Jest 0");
                 counter = picArray.length-1;
-                Log.d("Tomek", counter+"");
                 views.setImageViewResource(R.id.imageView4, picArray[counter]);
                 AppWidgetManager.getInstance(context).updateAppWidget(new ComponentName(context, listWidget.class), views);
             }else{
-                Log.d("Tomek", "Jest inna niz 0");
                 counter = counter-1;
                 views.setImageViewResource(R.id.imageView4, picArray[counter]);
                 AppWidgetManager.getInstance(context).updateAppWidget(new ComponentName(context, listWidget.class), views);
@@ -103,9 +105,7 @@ public class listWidget extends AppWidgetProvider {
                 views.setImageViewResource(R.id.imageView4, picArray[counter]);
                 AppWidgetManager.getInstance(context).updateAppWidget(new ComponentName(context, listWidget.class), views);
             }else{
-                Log.d("Tomek", "Jest inna niz 0 przy nastepnym. przed zmiana: " + counter);
                 counter = counter + 1;
-                Log.d("Tomek", "Jest inna niz 0 przy nastepnym. po zmianie: " + counter);
                 views.setImageViewResource(R.id.imageView4, picArray[counter]);
                 AppWidgetManager.getInstance(context).updateAppWidget(new ComponentName(context, listWidget.class), views);
             }
@@ -113,15 +113,73 @@ public class listWidget extends AppWidgetProvider {
         }
         if (MyOnClick4.equals(intent.getAction())){
             if(mPlay.isPlaying()){
+                views.setTextViewText(R.id.btPlayMusic, "Play");
+                AppWidgetManager.getInstance(context).updateAppWidget(new ComponentName(context, listWidget.class), views);
                 mPlay.pause();
             } else {
+                views.setTextViewText(R.id.btPlayMusic, "Pause");
+                AppWidgetManager.getInstance(context).updateAppWidget(new ComponentName(context, listWidget.class), views);
                 mPlay.start();
             }
         }
         if (MyOnClick5.equals(intent.getAction())){
             if(mPlay.isPlaying()){
+                views.setTextViewText(R.id.btPlayMusic, "Play");
+                AppWidgetManager.getInstance(context).updateAppWidget(new ComponentName(context, listWidget.class), views);
                 mPlay.stop();
-                mPlay = MediaPlayer.create(context, R.raw.drums);
+                mPlay = MediaPlayer.create(context, soundsArray[counterMusic]);
+            }
+        }
+        if (MyOnClick6.equals(intent.getAction())){
+            if(counterMusic == 0){
+                int i = 0;
+                if(mPlay.isPlaying()){
+                    i = 1;
+                    mPlay.stop();
+                }
+                counterMusic = soundsArray.length-1;
+                mPlay = MediaPlayer.create(context, soundsArray[counterMusic]);
+                if(i ==1 ){
+                    mPlay.start();
+                }
+            }else{
+                int i = 0;
+                if(mPlay.isPlaying()){
+                    i = 1;
+                    mPlay.stop();
+                }
+                counterMusic = counterMusic - 1;
+                mPlay = MediaPlayer.create(context, soundsArray[counterMusic]);
+
+                if(i ==1 ){
+                    mPlay.start();
+                }
+            }
+        }
+        if (MyOnClick7.equals(intent.getAction())){
+            if(counterMusic == soundsArray.length-1){
+                int i = 0;
+                if(mPlay.isPlaying()){
+                    i = 1;
+                    mPlay.stop();
+                }
+                counterMusic = 0;
+                mPlay = MediaPlayer.create(context, soundsArray[counterMusic]);
+                if(i ==1 ){
+                    mPlay.start();
+                }
+            }else{
+                int i = 0;
+                if(mPlay.isPlaying()){
+                    i = 1;
+                    mPlay.stop();
+                }
+                counterMusic = counterMusic + 1;
+                mPlay = MediaPlayer.create(context, soundsArray[counterMusic]);
+
+                if(i ==1 ){
+                    mPlay.start();
+                }
             }
         }
     }
